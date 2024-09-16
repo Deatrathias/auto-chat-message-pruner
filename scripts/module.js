d1b1ceb53c73560860cf51ba40b05955e5848ced
cadowtin <chasdowtin@gmail.com>
2024-09-16 13:05:25 -0500

    Fix so chat messages match up exactly

--- scripts/module.js
+++ scripts/module.js
@@ -11,12 +11,12 @@ Hooks.once('init', async function () {
 
 Hooks.once('ready', async function () {
     if (!game.user.isGM) return;
-    Hooks.on('preCreateChatMessage', async (_document, _data, _options, userId) => {
+    Hooks.on('preCreateChatMessage', async (_document, _data, _options, _userId) => {
         const maxMessages = game.settings.get("depruner-chat-message-remover", "limit");
         const messages = game.messages.contents;
         const messagesToDelete = messages.length - maxMessages;
         if (messagesToDelete <= 0) return;
-        for (let i = 0; i < messagesToDelete; i++) {
+        for (let i = 0; i <= messagesToDelete; i++) {
             const oldestMessage = messages[i];
             await oldestMessage.delete();
         }
